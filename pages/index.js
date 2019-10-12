def45a2bc6b22fa9f9d7725ecc59fe0576393773
Sarah Ransohoff <sranso@gmail.com>
2019-10-12 16:05:41 -0400

    Move form and question to own files.

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,10 @@
-import { useState } from 'react';
-import nlp from 'compromise';
 import Layout from '../components/Layout';
+import Form from '../components/Form';
 
-const handleSubmit = (e, value) => {
-  e.preventDefault();
-  const question = nlp(value);
-  console.log('q', question);
-};
-
-const Index = () => {
-  const [value, handleValueChange] = useState('');
-
-  return (
-    <Layout>
-      <p>Hello, it's good to see you. What can I help you with today?</p>
-      <form onSubmit={(e) => handleSubmit(e, value)}>
-        <label htmlFor="question"></label>
-        <textarea
-          type="text"
-          name="question"
-          id="question"
-          value={value}
-          onChange={(e) => handleValueChange(e.target.value)}
-          required>
-        </textarea>
-       <input type="submit" value="Let's see..."/>
-      </form>
-    </Layout>
-  );
-};
+const Index = () => (
+  <Layout>
+    <Form />
+  </Layout>
+);
 
 export default Index;
