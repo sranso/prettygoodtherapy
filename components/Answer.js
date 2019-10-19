5e0d8f96bb6446066233b9a28510a9e3886006e2
Sarah Ransohoff <sranso@gmail.com>
2019-10-19 16:49:01 -0400

    Add first feature test and answer the question.

---
+++ components/Answer.js
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+const Answer = ({ question, answer, askAgain }) => (
+  <div>
+    <p><i>{question}</i></p>
+    <p className="-answer">{answer}</p>
+    <button onClick={(e) => { e.preventDefault(); askAgain(); }}>ask again?</button>
+  </div>
+)
+
+export default Answer
--- components/Form.js
+++ components/Form.js
@@ -1,26 +1,19 @@
-import { useState } from 'react';
-import handleQuestion from '../lib/handleQuestion';
+const Form = ({ value, handleValueChange, onSubmit }) => (
+  <div>
+    <p>Hello, it's good to see you. What can I help you with today?</p>
+    <form onSubmit={(e) => { e.preventDefault(); onSubmit(); }}>
+      <label htmlFor="question"></label>
+      <textarea
+        type="text"
+        name="question"
+        id="question"
+        value={value}
+        onChange={(e) => handleValueChange(e.target.value)}
+        required>
+      </textarea>
+      <input type="submit" value="Let's see..."/>
+    </form>
+  </div>
+)
 
-const Form = () => {
-  const [value, handleValueChange] = useState('');
-
-  return (
-    <div>
-      <p>Hello, it's good to see you. What can I help you with today?</p>
-      <form onSubmit={(e) => { e.preventDefault(); handleQuestion(value); }}>
-        <label htmlFor="question"></label>
-        <textarea
-          type="text"
-          name="question"
-          id="question"
-          value={value}
-          onChange={(e) => handleValueChange(e.target.value)}
-          required>
-        </textarea>
-        <input type="submit" value="Let's see..."/>
-      </form>
-    </div>
-  );
-};
-
-export default Form;
+export default Form
